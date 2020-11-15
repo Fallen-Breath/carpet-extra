efd5e28c74f1c5d5250909e6998caba830a84eca
Fallen_Breath <yqx791125833@gmail.com>
2020-11-15 13:12:48 +0800

    3rd try

--- src/main/java/carpetextra/CarpetExtraServer.java
+++ src/main/java/carpetextra/CarpetExtraServer.java
@@ -2,6 +2,11 @@ package carpetextra;
 
 import carpet.CarpetExtension;
 import carpet.CarpetServer;
+import carpet.CarpetSettings;
+import carpet.settings.ParsedRule;
+import carpet.settings.SettingsManager;
+import carpet.settings.Validator;
+import carpet.utils.Translations;
 import carpetextra.commands.PingCommand;
 import carpetextra.utils.CarpetExtraTranslations;
 import com.mojang.brigadier.CommandDispatcher;
@@ -9,7 +14,11 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 
+import java.io.PrintStream;
+import java.util.Locale;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class CarpetExtraServer implements CarpetExtension
 {
@@ -78,5 +87,42 @@ public class CarpetExtraServer implements CarpetExtension
     public Map<String, String> canHasTranslations(String lang)
     {
         return CarpetExtraTranslations.getTranslationFromResourcePath(lang);
+    }
+
+    public static int printAllRulesToLog(String category)
+    {
+        CarpetSettings.language = "zh_cn";
+        Translations.updateLanguage(null);
+        PrintStream ps = System.out;
+        ps.println("# Carpet Extra 设置");
+        for (ParsedRule<?> rule : CarpetServer.settingsManager.getRules())
+        {
+            if (category != null && !rule.categories.contains(category))
+                continue;
+            ps.println("## " + rule.translatedName());
+            ps.println(rule.translatedDescription() + "  ");
+            for (String extra : rule.translatedExtras())
+                ps.println(extra + "  ");
+            ps.println("* 类型: `" + rule.type.getSimpleName() + "`  ");
+            ps.println("* 默认值: `" + rule.defaultAsString + "`  ");
+            String optionString = rule.options.stream().map(s -> "`" + s + "`").collect(Collectors.joining(", "));
+            ps.println((rule.isStrict ? "* 要求" : "* 建议") + "选项: " + optionString + "  ");
+            ps.println("* 分类: " + rule.categories.stream().map(s -> "`" + s.toUpperCase(Locale.ROOT) + "`").collect(Collectors.joining(", ")) + "  ");
+            boolean preamble = false;
+            for (Validator<?> validator : rule.validators)
+            {
+                if (validator.description() != null)
+                {
+                    if (!preamble)
+                    {
+                        ps.println("* 附加说明:  ");
+                        preamble = true;
+                    }
+                    ps.println("  * " + validator.description() + "  ");
+                }
+            }
+            ps.println("  ");
+        }
+        return 1;
     }
 }
